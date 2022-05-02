b1ea276a6a6aa5d32003ee85bc1125517ffc572e
BaraaSoft <baraaabuzeid@hotmail.com>
2022-05-03 00:47:57 +0200

    connect chart to main screen

--- src/screens/Main.screen.js
+++ src/screens/Main.screen.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import {Layout} from 'antd';
-import Header from '../components/Header'
+import Header from '../components/Header';
+import Chart from '../components/Chart'
 const {Content, Footer,Sider } = Layout;
 
 const MainScreen = ()=>{
 
     return(
-        <Layout style={{display:'flex',flexDirection:'column',height:'100hv'}} >
-        
-                  <Header style={{flex:1}}/>
-            <Layout style={{flex:4,alignSelf:'stretch'}} >
-                <Content>
-                    main content
+        <Layout  style={{height:'100vh',display:'flex',flexDirection:'column'}}>
+            <Header/>  
+            <Layout style={{flex:1}}>
+                <Content style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
+                    <Chart/>
                 </Content>
             </Layout>
-            <Footer style={{height:'60px'}} >footer</Footer>
-         
-          
+            <Footer style={{height:'60px'}}>footer</Footer>
         </Layout>
     )
 }
