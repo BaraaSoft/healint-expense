964548e30b7c1fd8effd063a39468940304bfc26
BaraaSoft <baraaabuzeid@hotmail.com>
2022-05-03 00:46:50 +0200

    add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,14 +6,12 @@ const Header = ()=>{
     return (
         <PageHeader
             ghost={false}
-            onBack={() => window.history.back()}
-            title="Title"
-            subTitle="This is a subtitle"
+            title="My Expense"
+            subTitle="To track your expenses with ease"
             extra={[
-                <Button key="3">Operation</Button>,
-                <Button key="2">Operation</Button>,
-                <Button key="1" type="primary">Primary</Button>,
-            ]}></PageHeader>
+                <Button key="1" type="primary">Add Expense</Button>,
+            ]}>
+        </PageHeader>
     )
 }
 
